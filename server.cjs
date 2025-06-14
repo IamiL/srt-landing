e179735bb04c026e8e14a3d40973dbcf5d99d5dd
Ivan Lebedev <iamil50113@gmail.com>
2025-06-14 22:57:36 +0300

    update

--- server.cjs
+++ server.cjs
@@ -2,6 +2,8 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const formidable = require("formidable");
+const FormData = require("form-data");
+const axios = require("axios");
 
 // const TELEGRAM_BOT_TOKEN = '7634754756:AAFbVy0Eujw-rzcfeSvi4sAp1QU74znblXA';
 // const TELEGRAM_USER_ID_FIRST = -1002888150428; // Первый канал/чат (хранилище)
@@ -98,173 +100,206 @@ const mimeTypes = {
     '.ttf': 'font/ttf'
 };
 
+async function sendFileToTelegram(filePath, userId) {
+    const form = new FormData();
+    form.append('chat_id', userId);
+    form.append('document', fs.createReadStream(filePath));
+    // form.append('disable_notification', true)
+
+    const response = await axios.post(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendDocument`, form, {
+        headers: {
+            ...form.getHeaders(),
+        },
+    });
+
+    return response.data.result.document.file_id; // возвращаем file_id
+}
+
+async function sendFileToTelegramById(fileId, chatId) {
+    const response = await axios.post(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendDocument`, {
+        chat_id: chatId,
+        document: fileId,
+    });
+
+    return await response.data.result; // Вы можете вернуть результат, если нужно
+}
+
+async function sendMessageToTelegram(chatId, text,) {
+    const response = await axios.post(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`, {
+        chat_id: chatId,
+        text: text,
+    });
+
+    return await response.data.result;
+}
+
 const server = http.createServer((req, res) => {
-    // if (req.url === '/api/file' && req.method === 'POST') {
-    //
-    //     const form = new formidable.IncomingForm();
-    //
-    //     // Логируем все поля и файлы
-    //     // form.on('field', (name, value) => {
-    //     //     console.log('Поле:', name, value);
-    //     // });
-    //
-    //     // form.on('file', (name, file) => {
-    //     //     console.log('Получен файл:', {
-    //     //         fieldName: name,
-    //     //         originalName: file.originalFilename,
-    //     //         tempPath: file.filepath,
-    //     //         size: file.size,
-    //     //         mimeType: file.mimetype
-    //     //     });
-    //     // });
-    //
-    //     form.on('error', (err) => {
-    //         console.error('Ошибка formidable:', err);
-    //     });
-    //
-    //     form.parse(req, (err, fields, files) => {
-    //         if (err) {
-    //             console.error('Ошибка парсинга:', err);
-    //             res.writeHead(500, {'Content-Type': 'application/json'});
-    //             return res.end(JSON.stringify({error: 'Server error'}));
-    //         }
-    //
-    //         // console.log('Parsed fields:', fields);
-    //         // console.log('Parsed files:', files);
-    //
-    //         if (!files.file) { // Теперь ожидаем поле 'file'
-    //             console.error('Файл не найден в запросе');
-    //             res.writeHead(400, {'Content-Type': 'application/json'});
-    //             return res.end(JSON.stringify({error: 'File is required'}));
-    //         }
-    //
-    //         const receivedFile = files.file[0];
-    //         console.log('Успешно принят файл:', receivedFile.originalFilename);
-    //         console.log('Размер:', receivedFile.size);
-    //
-    //         const filePath = receivedFile.originalFilename;
-    //
-    //         const MAX_FILE_SIZE = 50 * 1024 * 1024; // 10MB
-    //         if (receivedFile.size > MAX_FILE_SIZE) {
-    //             res.writeHead(413, {'Content-Type': 'application/json'});
-    //             return res.end(JSON.stringify({error: 'Файл слишком большой'}));
-    //         }
-    //
-    //         // Читаем временный файл
-    //         fs.readFile(receivedFile.filepath, (err, data) => {
-    //             if (err) {
-    //                 console.error('Ошибка чтения временного файла:', err);
-    //                 res.writeHead(500, {'Content-Type': 'application/json'});
-    //                 res.end(JSON.stringify({error: 'Ошибка чтения файла'}));
-    //                 return;
-    //             }
-    //
-    //             const savePath = path.join(__dirname, receivedFile.originalFilename);
-    //
-    //             fs.writeFile(savePath, data, async (err) => {
-    //                 if (err) {
-    //                     console.error('Ошибка сохранения файла:', err);
-    //                     res.writeHead(500, {'Content-Type': 'application/json'});
-    //                     res.end(JSON.stringify({error: 'Ошибка сохранения файла'}));
-    //                     return;
-    //                 }
-    //
-    //                 try {
-    //                     const fileId = await sendFileToTelegram(filePath, TELEGRAM_USER_ID_FIRST);
-    //                     // await sendFileToTelegramById(fileId, TELEGRAM_USER_ID_SECOND); // Отправка второму пользователю
-    //                     await new Promise(resolve => setTimeout(resolve, 20000)); // 2 секунды ожидания
-    //                     fs.unlink(savePath, (unlinkError) => {
-    //                         if (unlinkError) {
-    //                             console.error('Ошибка удаления файла:', unlinkError);
-    //                             // Даже если не удалось удалить, продолжаем
-    //                         } else {
-    //                             console.log('Файл успешно удален:', savePath);
-    //                             res.writeHead(200, {'Content-Type': 'application/json'});
-    //                             res.end(JSON.stringify({fileId}));
-    //                         }
-    //                     })
-    //                 } catch (error) {
-    //                     console.error('Ошибка при отправке файла в Telegram:', error);
-    //                     res.writeHead(500, {'Content-Type': 'text/plain'});
-    //                     res.end('Ошибка при отправке файла в Telegram');
-    //
-    //                     fs.unlink(savePath, (unlinkError) => {
-    //                         if (unlinkError) {
-    //                             console.error('Ошибка удаления файла:', unlinkError);
-    //                             // Даже если не удалось удалить, продолжаем
-    //                         }
-    //                     })
-    //                 }
-    //
-    //             });
-    //
-    //
-    //         });
-    //
-    //         // res.writeHead(200, {'Content-Type': 'application/json'});
-    //         // res.end(JSON.stringify({
-    //         //     success: true,
-    //         //     filename: uploadedFile.originalFilename,
-    //         //     size: uploadedFile.size
-    //         // }));
-    //     });
-    //     return;
-    // }
-    //
-    // if (req.url === '/api/application' && req.method === 'POST') {
-    //     // Проверяем Content-Type (должен быть application/json)
-    //     if (req.headers['content-type'] !== 'application/json') {
-    //         res.writeHead(400, {'Content-Type': 'application/json'});
-    //         res.end(JSON.stringify({error: 'Требуется Content-Type: application/json'}));
-    //         return;
-    //     }
-    //
-    //     // Читаем тело запроса целиком (если оно небольшое)
-    //     const chunks = [];
-    //     req.on('data', (chunk) => chunks.push(chunk));
-    //     req.on('end', async () => {
-    //         try {
-    //             const jsonData = JSON.parse(Buffer.concat(chunks).toString());
-    //             console.log('✅ Получен JSON:', jsonData);
-    //
-    //             let filesString = ''
-    //
-    //             for (let i = 0; i < jsonData.filesArray.length; i++) {
-    //                 filesString += `\n${i + 1}. ${jsonData.filesArray[i].name}`;
-    //             }
-    //
-    //             console.log('список файлов: ', filesString);
-    //
-    //             const msg = `Новый запрос.\nИмя: ${jsonData.name}\nТелефон: ${jsonData.phone}\nПочта: ${jsonData.email}\nФайлов прикреплено: ${jsonData.filesArray ? jsonData.filesArray.length : '0'}\nОписание заявки: ${jsonData.description}\nФайлы: ${filesString}`
-    //
-    //             await sendMessageToTelegram(TELEGRAM_REQUESTS_CHAT_ID, msg);
-    //
-    //             //sendMessageToTelegram(TELEGRAM_REQUESTS_CHAT_ID, msg)
-    //             for (const fileData of jsonData.filesArray) {
-    //                 await sendFileToTelegramById(fileData.tgId, TELEGRAM_REQUESTS_CHAT_ID)
-    //             }
-    //             // Отвечаем успехом (200 OK)
-    //             res.writeHead(200, {'Content-Type': 'application/json'});
-    //             res.end(JSON.stringify({success: true, data: jsonData}));
-    //         } catch (err) {
-    //             console.error('❌ Ошибка парсинга JSON:', err);
-    //             res.writeHead(400, {'Content-Type': 'application/json'});
-    //             res.end(JSON.stringify({error: 'Невалидный JSON'}));
-    //         }
-    //     });
-    //     return;
-    // }
-
-
     if (req.url === '/api/file' && req.method === 'POST') {
-        console.log("отправка файла");
+
+        const form = new formidable.IncomingForm();
+
+        // Логируем все поля и файлы
+        // form.on('field', (name, value) => {
+        //     console.log('Поле:', name, value);
+        // });
+
+        // form.on('file', (name, file) => {
+        //     console.log('Получен файл:', {
+        //         fieldName: name,
+        //         originalName: file.originalFilename,
+        //         tempPath: file.filepath,
+        //         size: file.size,
+        //         mimeType: file.mimetype
+        //     });
+        // });
+
+        form.on('error', (err) => {
+            console.error('Ошибка formidable:', err);
+        });
+
+        form.parse(req, (err, fields, files) => {
+            if (err) {
+                console.error('Ошибка парсинга:', err);
+                res.writeHead(500, {'Content-Type': 'application/json'});
+                return res.end(JSON.stringify({error: 'Server error'}));
+            }
+
+            // console.log('Parsed fields:', fields);
+            // console.log('Parsed files:', files);
+
+            if (!files.file) { // Теперь ожидаем поле 'file'
+                console.error('Файл не найден в запросе');
+                res.writeHead(400, {'Content-Type': 'application/json'});
+                return res.end(JSON.stringify({error: 'File is required'}));
+            }
+
+            const receivedFile = files.file[0];
+            console.log('Успешно принят файл:', receivedFile.originalFilename);
+            console.log('Размер:', receivedFile.size);
+
+            const filePath = receivedFile.originalFilename;
+
+            const MAX_FILE_SIZE = 50 * 1024 * 1024; // 10MB
+            if (receivedFile.size > MAX_FILE_SIZE) {
+                res.writeHead(413, {'Content-Type': 'application/json'});
+                return res.end(JSON.stringify({error: 'Файл слишком большой'}));
+            }
+
+            // Читаем временный файл
+            fs.readFile(receivedFile.filepath, (err, data) => {
+                if (err) {
+                    console.error('Ошибка чтения временного файла:', err);
+                    res.writeHead(500, {'Content-Type': 'application/json'});
+                    res.end(JSON.stringify({error: 'Ошибка чтения файла'}));
+                    return;
+                }
+
+                const savePath = path.join(__dirname, receivedFile.originalFilename);
+
+                fs.writeFile(savePath, data, async (err) => {
+                    if (err) {
+                        console.error('Ошибка сохранения файла:', err);
+                        res.writeHead(500, {'Content-Type': 'application/json'});
+                        res.end(JSON.stringify({error: 'Ошибка сохранения файла'}));
+                        return;
+                    }
+
+                    try {
+                        const fileId = await sendFileToTelegram(filePath, TELEGRAM_USER_ID_FIRST);
+                        // await sendFileToTelegramById(fileId, TELEGRAM_USER_ID_SECOND); // Отправка второму пользователю
+                        await new Promise(resolve => setTimeout(resolve, 20000)); // 2 секунды ожидания
+                        fs.unlink(savePath, (unlinkError) => {
+                            if (unlinkError) {
+                                console.error('Ошибка удаления файла:', unlinkError);
+                                // Даже если не удалось удалить, продолжаем
+                            } else {
+                                console.log('Файл успешно удален:', savePath);
+                                res.writeHead(200, {'Content-Type': 'application/json'});
+                                res.end(JSON.stringify({fileId}));
+                            }
+                        })
+                    } catch (error) {
+                        console.error('Ошибка при отправке файла в Telegram:', error);
+                        res.writeHead(500, {'Content-Type': 'text/plain'});
+                        res.end('Ошибка при отправке файла в Telegram');
+
+                        fs.unlink(savePath, (unlinkError) => {
+                            if (unlinkError) {
+                                console.error('Ошибка удаления файла:', unlinkError);
+                                // Даже если не удалось удалить, продолжаем
+                            }
+                        })
+                    }
+
+                });
+
+
+            });
+
+            // res.writeHead(200, {'Content-Type': 'application/json'});
+            // res.end(JSON.stringify({
+            //     success: true,
+            //     filename: uploadedFile.originalFilename,
+            //     size: uploadedFile.size
+            // }));
+        });
         return;
     }
 
     if (req.url === '/api/application' && req.method === 'POST') {
-        console.log("отправка формы");
-        return
+        // Проверяем Content-Type (должен быть application/json)
+        if (req.headers['content-type'] !== 'application/json') {
+            res.writeHead(400, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify({error: 'Требуется Content-Type: application/json'}));
+            return;
+        }
+
+        // Читаем тело запроса целиком (если оно небольшое)
+        const chunks = [];
+        req.on('data', (chunk) => chunks.push(chunk));
+        req.on('end', async () => {
+            try {
+                const jsonData = JSON.parse(Buffer.concat(chunks).toString());
+                console.log('✅ Получен JSON:', jsonData);
+
+                let filesString = ''
+
+                for (let i = 0; i < jsonData.filesArray.length; i++) {
+                    filesString += `\n${i + 1}. ${jsonData.filesArray[i].name}`;
+                }
+
+                console.log('список файлов: ', filesString);
+
+                const msg = `Новый запрос.\nИмя: ${jsonData.name}\nТелефон: ${jsonData.phone}\nПочта: ${jsonData.email}\nФайлов прикреплено: ${jsonData.filesArray ? jsonData.filesArray.length : '0'}\nОписание заявки: ${jsonData.description}\nФайлы: ${filesString}`
+
+                await sendMessageToTelegram(TELEGRAM_REQUESTS_CHAT_ID, msg);
+
+                //sendMessageToTelegram(TELEGRAM_REQUESTS_CHAT_ID, msg)
+                for (const fileData of jsonData.filesArray) {
+                    await sendFileToTelegramById(fileData.tgId, TELEGRAM_REQUESTS_CHAT_ID)
+                }
+                // Отвечаем успехом (200 OK)
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                res.end(JSON.stringify({success: true, data: jsonData}));
+            } catch (err) {
+                console.error('❌ Ошибка парсинга JSON:', err);
+                res.writeHead(400, {'Content-Type': 'application/json'});
+                res.end(JSON.stringify({error: 'Невалидный JSON'}));
+            }
+        });
+        return;
     }
+
+
+    // if (req.url === '/api/file' && req.method === 'POST') {
+    //     console.log("отправка файла");
+    //     return;
+    // }
+    //
+    // if (req.url === '/api/application' && req.method === 'POST') {
+    //     console.log("отправка формы");
+    //     return
+    // }
 
     const requestStartTime = Date.now();
     const timestamp = getTimestamp();
