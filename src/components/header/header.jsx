d15ddf25c14d69a087d231848f41699d207303bf
Ivan Lebedev <iamil50113@gmail.com>
2025-06-15 05:01:13 +0300

    update

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,16 +3,26 @@ import Navigation from "../navigation/navigation.jsx";
 import "./header.css"
 import MobileMenu from "../mobileMenu/menu.jsx";
 import Logo from "../logo/logo.jsx";
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 
 export default function Header({enLan}) {
-    return <header id={styles.header} className="base_grid">
-        <Link to='/' id={styles.logo}>
-            <Logo enLan={enLan}/>
+    const location = useLocation();
+    const isHomePage = location.pathname === '/';
+
+    return <header
+            id={styles.header}
+            className="base_grid"
+    >
+        <Link
+                to='/'
+                id={styles.logo}
+                className={`${isHomePage ? "header-main-logo-start-animation" : ""}`}
+        >
+            <Logo enLan={enLan} />
         </Link>
-        <div className='nav_menu'>
-            <Navigation enLan={enLan}/>
-            <MobileMenu enLan={enLan}/>
+        <div className={`nav_menu ${isHomePage ? 'nav_menu_start_animated' : ''}`}>
+            <Navigation enLan={enLan} />
+            <MobileMenu enLan={enLan} />
         </div>
     </header>
 }
